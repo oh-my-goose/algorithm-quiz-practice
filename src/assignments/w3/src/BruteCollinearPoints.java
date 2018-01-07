d4d11384b563e287d1a33990d0f8094c8fa1eb30
Boy Wang <boyw165@gmail.com>
2018-01-07 15:15:34 +0800

    Add week 3 assignment.

---
+++ src/assignments/w3/src/BruteCollinearPoints.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BruteCollinearPoints {
+
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    public LineSegment[] segments() {
+        return null;
+    }
+}
