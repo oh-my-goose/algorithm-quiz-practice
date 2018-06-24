9d4a52b2778a48a56992f26fa933597519d08343
Boy Wang <boyw165@gmail.com>
2018-06-24 22:36:31 +0800

    Add MergeInterval.kt.

--- src/quiz/src/medium/MergeIntervals.kt
+++ src/quiz/src/medium/MergeIntervals.kt
@@ -2,9 +2,6 @@ package medium
 
 import org.junit.Assert
 
-import java.util.Locale
-import java.util.Stack
-
 /**
  * Given a collection of intervals, merge all overlapping intervals.
  *
@@ -30,16 +27,101 @@ class MergeIntervals {
     // Solution #1 ////////////////////////////////////////////////////////////
 
     fun merge(intervals: List<Interval>): List<Interval> {
-        TODO()
+        if (intervals.isEmpty()) return emptyList()
+        else if (intervals.size == 1) return intervals
+
+        // Sort in the ascending order of interval's start
+        intervals.sortedWith(Comparator { a, b ->
+            return@Comparator when {
+                a.start > b.start -> -1
+                a.start < b.start -> 1
+                else -> 0
+            }
+        })
+
+        val mergedIntervals = mutableListOf<Interval>()
+        var start = intervals[0].start
+        var end = intervals[0].end
+        for (i in 1..intervals.lastIndex) {
+            val current = intervals[i]
+
+            if (current.start <= end) {
+                // Extend the interval because there is a overlap
+                start = Math.min(start, current.start)
+                end = Math.max(end, current.end)
+            } else {
+                // No overlap found, add new interval
+                mergedIntervals.add(Interval(start = start, end = end))
+                // Reset the cache
+                start = current.start
+                end = current.end
+            }
+        }
+
+        mergedIntervals.add(Interval(start = start, end = end))
+
+        return mergedIntervals
     }
 
-    class Interval(var start: Int = 0,
-                   var end: Int = 0)
+    data class Interval(var start: Int = 0,
+                        var end: Int = 0) {
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Interval
+
+            if (start != other.start) return false
+            if (end != other.end) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = start
+            result = 31 * result + end
+            return result
+        }
+    }
 
     companion object {
 
         @JvmStatic
         fun main(args: Array<String>) {
+            val tester = MergeIntervals()
+
+            // Input:
+            // [-------------------]
+            //       [-----]
+            // +-+-+-+-+-+-+-+-+-+-+-...
+            // 0 1 2 3 4 5 6 7 8 9 10
+            Assert.assertEquals(
+                listOf(Interval(start = 1, end = 10)),
+                tester.merge(listOf(
+                    Interval(start = 1, end = 10),
+                    Interval(start = 3, end = 6))))
+
+            // Input:
+            //   [---]
+            //             [-]
+            //           [-]
+            //                 [-]
+            //                 [-]
+            //                   [-]
+            // +-+-+-+-+-+-+-+-+-+-+-...
+            // 0 1 2 3 4 5 6 7 8 9 10
+            Assert.assertEquals(
+                listOf(Interval(start = 1, end = 3),
+                       Interval(start = 5, end = 7),
+                       Interval(start = 8, end = 10)),
+                tester.merge(listOf(
+                    Interval(start = 1, end = 3),
+                    Interval(start = 6, end = 7),
+                    Interval(start = 5, end = 6),
+                    Interval(start = 8, end = 9),
+                    Interval(start = 8, end = 9),
+                    Interval(start = 9, end = 10))))
         }
     }
 }
